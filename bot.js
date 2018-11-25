8ef6fe08791b9c485017ee089207b37a6590a205
TryAgain911 <emulateurs@hotmail.fr>
2018-11-25 20:02:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const ytdl = require("ytdl-core");
+const request = require("request");
+const fs = require("fs");
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("yourtube-info");
+
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    /*if (message.content === '?ping') {
+    if (message.content === '?ping') {
     	message.reply('pong');
   	}
-    */
-    if (message.content === 'Bonjour') {
-    	message.reply('Salutations !');
-  	}
     
-    if (message.content.startsWith('?play')) {
-    // On récupère le premier channel audio du serveur
-    let voiceChannel = message.guild.channels
-      .filter(function (channel) { return channel.type === 'voice' })
-      .first()
-    // On récupère les arguments de la commande 
-    // il faudrait utiliser une expression régulière pour valider le lien youtube
-    let args = message.content.split(' ')
-    // On rejoint le channel audio
-    voiceChannel
-      .join()
-      .then(function (connection) {
-        // On démarre un stream à partir de la vidéo youtube
-        let stream = YoutubeStream(args[1])
-        stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
-        // On envoie le stream au channel audio
-        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-        connection
-          .playStream(stream)
-          .on('end', function () {
-            connection.disconnect()
-          })
-      })
-  }
     
 });
 
